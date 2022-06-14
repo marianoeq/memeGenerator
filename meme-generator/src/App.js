b92c353b29e8739144df3e56f9327764b2cd260a
marian.eq <mariano.eq@gmail.com>
2022-06-14 12:10:11 +0200

    Meme component added

--- meme-generator/src/App.js
+++ meme-generator/src/App.js
@@ -1,9 +1,13 @@
-import './App.css';
+import React from 'react'
+import Header from "./Components/Header";
+import Meme from "./Components/Meme";
 
 function App() {
+ 
   return (
-    <div className="App">
-      
+    <div>
+      <Header/>
+      <Meme/>
     </div>
   );
 }
