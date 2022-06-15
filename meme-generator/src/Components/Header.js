412cbb435d4fe60db9f20a827fd2042964c1b937
marian.eq <mariano.eq@gmail.com>
2022-06-15 15:21:55 +0200

    dark mode toggle added

--- meme-generator/src/Components/Header.js
+++ meme-generator/src/Components/Header.js
@@ -1,15 +1,21 @@
 import React from "react";
 
-export default function Header() {
+export default function Header({darkMode, clickMe}) {
   return (
-    <header className="header">
+    <header className= {darkMode? "dark": ""}>
       <img
         src="./images/troll-face.png"
         className="header--image"
         alt="trolle"
       />
       <h2 className="header--title">Meme Generator</h2>
-      <h4 className="header--project">React Project </h4>
+      <div className="toggler">
+        <p className="toggler--light">Light</p>
+        <div className="toggler--slider" onClick={clickMe} >
+          <div className="toggler--slider--circle"></div>
+        </div>
+        <p className="toggler--dark">Dark</p>
+      </div>
     </header>
   );
 }
