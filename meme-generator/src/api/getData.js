aca5a7b9fc0f745b5c4440289e0dc60eb3971d08
marian.eq <mariano.eq@gmail.com>
2022-06-15 15:26:21 +0200

    files no longer in used

--- meme-generator/src/api/getData.js
+++
@@ -1,7 +0,0 @@
-const getdata = async() => {
-    const response = await fetch("https://swapi.dev/api/people")
-    const data = await response.json();
-    return data
-}
- console.log(data)
-export default getdata;
