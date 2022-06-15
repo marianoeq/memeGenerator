96107c45e11ab00e19c15029eb257567ba1077a1
marian.eq <mariano.eq@gmail.com>
2022-06-15 15:20:31 +0200

    dark mode function added used of state

--- meme-generator/src/App.js
+++ meme-generator/src/App.js
@@ -1,13 +1,18 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Header from "./Components/Header";
 import Meme from "./Components/Meme";
 
 function App() {
- 
+  const [dark, setDark] = useState(false)
+
+  function handleClick(){
+    setDark(prevDarkState => !prevDarkState)
+  }
+
   return (
     <div>
-      <Header/>
-      <Meme/>
+      <Header clickMe={handleClick} darkMode={dark}/>
+      <Meme darkMode={dark}/>
     </div>
   );
 }
